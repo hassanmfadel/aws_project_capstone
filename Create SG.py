bd229cc90cedfd5e58eb2cdd5b982b8bc6b778b1
hassanmfadel <hassan.m.fadel@gmail.com>
2024-06-24 05:37:40 +0300

    create SG and rules

---
+++ Create SG.py
@@ -0,0 +1,69 @@
+import boto3
+from pprint import pprint
+session=boto3.session.Session(profile_name='iam-admin',region_name='us-east-1')
+client_obj=session.client(service_name='ec2',region_name='us-east-1')
+resource_obj=boto3.resource(service_name='ec2',region_name='us-east-1')
+
+vpc_id='vpc-07def5e85a7b039ea'
+try:
+    response = client_obj.create_security_group(
+        Description='SG for aws project',
+        GroupName='web-sg',
+        VpcId=vpc_id)
+
+    response_sg = client_obj.describe_security_groups(
+        Filters=[
+            {
+                'Name': 'group-name',
+                'Values': ['web_sg']
+            }
+        ]
+    )
+    group_id=response_sg['SecurityGroups'][0]['GroupId']
+    #pprint(response_sg)
+    #print(group_id)
+
+
+    client_obj.authorize_security_group_ingress(
+            GroupId=group_id,
+            IpPermissions=[
+            {
+                'FromPort': 22,
+                'IpProtocol': 'tcp',
+                'IpRanges': [
+                    {
+                        'CidrIp': '0.0.0.0/24',
+                        'Description': 'SSH access from the users',
+                    },
+                ],
+                'ToPort': 22,
+            },],)
+    client_obj.authorize_security_group_ingress(
+            GroupId=group_id,
+            IpPermissions=[
+            {
+                'FromPort': 80,
+                'IpProtocol': 'tcp',
+                'IpRanges': [
+                    {
+                        'CidrIp': '0.0.0.0/24',
+                        'Description': 'Allow inbound https',
+                    },
+                ],
+                'ToPort': 80,
+            },],)
+    client_obj.authorize_security_group_ingress(
+            GroupId=group_id,
+            IpPermissions=[
+            {
+                'FromPort': 443,
+                'IpProtocol': 'tcp',
+                'IpRanges': [
+                    {
+                    'CidrIp': '0.0.0.0/24',
+                    'Description': 'allow inbound https',
+                    },],
+                'ToPort': 443,
+        },],)
+except Exception as e:
+    print(e)
